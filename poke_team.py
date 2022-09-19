4606038e0d8ce723c99f9d99097168d9e518ebae
huiiwen <hyap0016@student.monash.edu>
2022-09-20 00:46:05 +0800

    cry

--- poke_team.py
+++ poke_team.py
@@ -1,13 +1,19 @@
 from __future__ import annotations
 
-from .random_gen import RandomGen
+from random_gen import RandomGen
 
 """
 """
 __author__ = "Scaffold by Jackson Goerner, Code by ______________"
 
+# import files and libraries needed
 from enum import Enum, auto
-from pokemon_base import PokemonBase
+from pokemon_base import PokeType, PokemonBase
+from pokemon import Charmander, Bulbasaur, Squirtle, Gastly, Eevee
+from referential_array import ArrayR
+from stack_adt import Stack
+from queue_adt import Queue
+from sorted_list import SortedList
 
 class Action(Enum):
     ATTACK = auto()
@@ -36,13 +42,71 @@ class PokeTeam:
     
     @classmethod
     def random_team(cls, team_name: str, battle_mode: int, team_size=None, ai_mode=None, **kwargs):
-        RandomGen.randint()
+        if team_size == None:
+            team_size = RandomGen.randint(3,6)
 
+        team = []
+        for num in range (team_size+2):
+            if num == 0:
+                team.append(0)
+            elif num == team_size+1:
+                team.append(team_size)
+            else:
+                team.append(RandomGen.randint(0,team_size))
+
+        team.sort()
+        team_count = []
+        for pokemon in range(len(team)-1):
+            number = team[pokemon+1] - team[pokemon]
+            team_count.append(number)
+        team_count = team_count[2:]
+
+        pokemon_team = ArrayR(team_size)
+        pokemon_arranged = [Charmander, Bulbasaur, Squirtle, Gastly, Eevee]
+        i = 0
+        pokemon_total = 0
+        for index in range(len(team_count)):
+            if team_count[index] != 0:
+                pokemon_total += team_count[index]
+                number = 0
+                while number < team_count[index]:
+                    while i < pokemon_total:
+                        pokemon_team.setitem(i, pokemon_arranged[index])
+                        i += 1
+                    number += 1
+
+        return pokemon_team
+    
     def return_pokemon(self, poke: PokemonBase) -> None:
-        raise NotImplementedError()
+        if self.battle_mode == 0:
+            self.new_stack.push(poke)
+        elif self.battle_mode == 1:
+            self.new_queue.append(poke)
+        elif self.battle_mode == 2:
+            self.add(poke)
 
     def retrieve_pokemon(self) -> PokemonBase | None:
-        raise NotImplementedError()
+        if self.is_empty():
+            print("here")
+            return None
+        else:
+            print("there")
+            if self.battle_mode == 0:
+                self.new_stack = Stack() 
+                for pokemon in range((self.pokemon_team.__len__() - 1), -1, -1):
+                    self.new_stack.push(self.pokemon_team.__getitem__(pokemon))
+                retrieved_pokemon = self.new_stack.pop()
+            elif self.battle_mode == 1:
+                self.new_queue = Queue()
+                for pokemon in self.pokemon_team:
+                    self.new_queue.append(pokemon)
+                retrieved_pokemon = self.new_queue.serve()
+            elif self.battle_mode == 2:
+                new_sorted_list = SortedList()
+                for pokemon in range(len(self.pokemon_team)):
+                    pass
+
+            return retrieved_pokemon
 
     def special(self):
         raise NotImplementedError()
@@ -51,10 +115,13 @@ class PokeTeam:
         raise NotImplementedError()
 
     def __str__(self):
-        raise NotImplementedError()
+        poke_team_string = f"{self.team_name} ({self.team_numbers}): [{self.battle_mode}]"
+        return poke_team_string
 
+        #"Dawn (2): [LV. 1 Gastly: 6 HP, LV. 1 Squirtle: 11 HP, LV. 1 Eevee: 10 HP, LV. 1 Bulbasaur: 13 HP, LV. 1 Charmander: 9 HP]"
+    
     def is_empty(self):
-        raise NotImplementedError()
+        return self.__len__()
 
     def choose_battle_option(self, my_pokemon: PokemonBase, their_pokemon: PokemonBase) -> Action:
         raise NotImplementedError()
@@ -62,3 +129,16 @@ class PokeTeam:
     @classmethod
     def leaderboard_team(cls):
         raise NotImplementedError()
+
+if __name__ == "__main__":
+    RandomGen.set_seed(123456789)
+    t = PokeTeam.random_team("Cynthia", 0)
+    pokemon = []
+    while not t.is_empty():
+        pokemon.append(t.retrieve_pokemon())
+    expected_classes = [Squirtle, Gastly, Eevee, Eevee, Eevee, Eevee]
+    print(len(pokemon))
+    print(len(expected_classes))
+    for p, e in zip(pokemon, expected_classes):
+        print(p)
+        print(e)
--- pokemon.py
+++ pokemon.py
@@ -4,40 +4,140 @@ from pokemon_base import PokemonBase
 
 __author__ = "Scaffold by Jackson Goerner, Code by Elysia Chang"
 
-class Charizard(PokemonBase):
-    charizard_dict = {}
-    charizard_dict['Name'] = "Charizard"
-    charizard_dict['Type'] = "Fire"
-    charizard_dict['Base Level'] = 3
-    charizard_dict['HP'] = 12 + 1 * charizard_dict['Base Level']
+# class Charizard(PokemonBase):
+#     charizard_dict = {}
+#     charizard_dict['Name'] = "Charizard"
+#     charizard_dict['Type'] = "Fire"
+#     charizard_dict['Base Level'] = 3
+#     charizard_dict['HP'] = 12 + 1 * charizard_dict['Base Level']
     
 
-class Charmander:
-    pass
+class Charmander(PokemonBase):
+    def __init__(self):
+        self.poke_name = "Charmander"
+        self.poke_type = "Fire"
+        self.level = 1
+        self.hp = 8 + 1 * self.level
+        self.attack_damage = 6 + 1 * self.level
+        self.speed = 7 + 1 * self.level
+        self.defence = 4
+        # defence calculation
 
+        super().__init__(self.hp, self.poke_type)
 
-class Venusaur:
-    pass
+class Squirtle(PokemonBase):
+    def __init__(self):
+        self.poke_name = "Squirtle"
+        self.poke_type = "Water"
+        self.level = 1
+        self.hp = 9 + 2 * self.level
+        self.attack_damage = 4 + (self.level // 2)
+        self.speed = 7
+        self.defence = 6 + self.level
+        # defence calculation
 
-class Bulbasaur:
-    pass
+        super().__init__(self.hp, self.poke_type)
 
+class Bulbasaur(PokemonBase):
+    def __init__(self):
+        self.poke_name = "Bulbasaur"
+        self.poke_type = "Grass"
+        self.level = 1
+        self.hp = 12 + 1 * self.level
+        self.attack_damage = 5
+        self.speed = 7 + (self.level // 2)
+        self.defence = 5
+        # defence calculation
 
-class Blastoise:
-    pass
+        super().__init__(self.hp, self.poke_type)
 
-class Squirtle:
-    pass
+class Gastly(PokemonBase):
+    def __init__(self):
+        self.poke_name = "Gastly"
+        self.poke_type = "Ghost"
+        self.level = 1
+        self.hp = 6 + (self.level // 2)
+        self.attack_damage = 4
+        self.speed = 2
+        self.defence = 8
+        # defence calculation
 
-class Gengar:
-    pass
+        super().__init__(self.hp, self.poke_type)
 
-class Haunter:
-    pass
+class Eevee(PokemonBase):
+    def __init__(self):
+        self.poke_name = "Eevee"
+        self.poke_type = "Normal"
+        self.level = 1
+        self.hp = 10
+        self.attack_damage = 6 + self.level
+        self.speed = 7 + self.level
+        self.defence = 4 + self.level
+        # defence calculation
 
-class Gastly:
-    pass
+        super().__init__(self.hp, self.poke_type)
 
+class Charizard(PokemonBase):
+    def __init__(self):
+        self.poke_name = "Charizard"
+        self.poke_type = "Fire"
+        self.level = 3
+        self.hp = 12 + 1 * self.level
+        self.attack_damage = 10 + 2 * self.level
+        self.speed = 9 + 1 * self.level
+        self.defence = 4
+        # defence calculation
 
-class Eevee:
-    pass
+        super().__init__(self.hp, self.poke_type)
+
+class Blastoise(PokemonBase):
+    def __init__(self):
+        self.poke_name = "Blastoise"
+        self.poke_type = "Squirtle"
+        self.level = 3
+        self.hp = 15 + 2 * self.level
+        self.attack_damage = 8 + (self.level // 2)
+        self.speed = 10
+        self.defence = 8 + 1 * self.level
+        # defence calculation
+
+        super().__init__(self.hp, self.poke_type)
+
+class Venusaur(PokemonBase):
+    def __init__(self):
+        self.poke_name = "Venusaur"
+        self.poke_type = "Grass"
+        self.level = 2
+        self.hp = 20 + (self.level // 2)
+        self.attack_damage = 5
+        self.speed = 3 + (self.level // 2)
+        self.defence = 10
+        # defence calculation
+
+        super().__init__(self.hp, self.poke_type)
+
+class Haunter(PokemonBase):
+    def __init__(self):
+        self.poke_name = "Haunter"
+        self.poke_type = "Ghost"
+        self.level = 1
+        self.hp = 9 + (self.level // 2)
+        self.attack_damage = 8
+        self.speed = 6
+        self.defence = 6
+        # defence calculation
+
+        super().__init__(self.hp, self.poke_type)
+
+class Gengar(PokemonBase):
+    def __init__(self):
+        self.poke_name = "Gengar"
+        self.poke_type = "Ghost"
+        self.level = 3
+        self.hp = 12 + (self.level // 2)
+        self.attack_damage = 18
+        self.speed = 12
+        self.defence = 3
+        # defence calculation
+
+        super().__init__(self.hp, self.poke_type)
