c2b01aac1e63ac5cf4701201fe16ae0ce0f0a18e
hannahtma <101638126+hannahtma@users.noreply.github.com>
2022-09-26 00:46:55 +0800

    Updated battle.py
    Changed evolve back to get_evolved_version and reassigned to evolving pokemon

--- battle.py
+++ battle.py
@@ -38,16 +38,14 @@ class Battle:
                     print()
                 elif pokemon2.get_speed() > pokemon1.get_speed() and pokemon2.is_fainted() == False and pokemon2.is_fainted() == False:
                     print("pokemon2 attack first")
-                    print(pokemon2.get_hp())
                     print("pokemon1's hp before attack",pokemon1,pokemon1.get_hp())
                     pokemon2.attack(pokemon1)
                     print("pokemon2's attack damage",pokemon1.get_attack_damage())
                     print("pokemon1's hp after attack",pokemon1.get_hp())
                     print()
-                    print(pokemon2.get_status_effect())
-                    print(pokemon2.get_hp())
                     print("pokemon2's hp before attack",pokemon2,pokemon2.get_hp())
                     pokemon1.attack(pokemon2)
+                    print("did eevee take damage?")
                     print("pokemon1's attack damage",pokemon1.get_attack_damage())
                     print("pokemon2's hp after attack",pokemon2.get_hp())
                     print()
@@ -78,6 +76,9 @@ class Battle:
                 else:
                     pokemon2.heal()
             
+            print(pokemon1,"my status effect is: ",pokemon1.get_status_effect())
+            print(pokemon2,"my status effect is: ",pokemon2.get_status_effect())
+            
             if pokemon1.get_hp() > 0 and pokemon2.get_hp() > 0:
                 print("before pokemon1 lose 1 hp",pokemon1.get_hp())
                 pokemon1.lose_hp(1)
@@ -92,9 +93,9 @@ class Battle:
                 pokemon1.level_up()
 
             if pokemon1.is_fainted() == False and pokemon1.can_evolve() == True:
-                pokemon1.evolve()
+                pokemon1 = pokemon1.get_evolved_version()
             if pokemon2.is_fainted() == False and pokemon2.can_evolve() == True:
-                pokemon2.evolve()
+                pokemon2 = pokemon2.get_evolved_version()
 
             if pokemon1.is_fainted():
                 print("pokemon1 is dead")
@@ -102,16 +103,12 @@ class Battle:
                 print("so what is team1 new pokemon?",pokemon1)
             elif pokemon1.is_fainted() == False:
                 pokemon1.remove_status_effect()
-                team1.return_pokemon(pokemon1)
-                pokemon1 = team1.retrieve_pokemon()
             if pokemon2.is_fainted():
                 print("pokemon2 is dead")
                 pokemon2 = team2.retrieve_pokemon()
                 print("so what is team2 new pokemon?",pokemon2)
             elif pokemon2.is_fainted() == False:
                 pokemon2.remove_status_effect()
-                team2.return_pokemon(pokemon2)
-                pokemon2 = team2.retrieve_pokemon()
 
             counter += 1
         print("is team2 empty?",team2.is_empty())
@@ -150,7 +147,9 @@ if __name__ == "__main__":
 
     RandomGen.set_seed(192837465)
     team1 = PokeTeam("Brock", [1, 1, 1, 1, 1], 2, PokeTeam.AI.SWAP_ON_SUPER_EFFECTIVE, criterion=Criterion.HP)
+    print(team1)
     team2 = PokeTeam("Misty", [0, 0, 0, 3, 3], 2, PokeTeam.AI.SWAP_ON_SUPER_EFFECTIVE, criterion=Criterion.SPD)
+    print(team2)
     b = Battle(verbosity=0)
     res = b.battle(team1, team2)
     print(res) # 1
